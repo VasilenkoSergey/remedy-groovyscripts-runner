1eb1bd309030d019487a5011b84933dc5041fc22
Sergey Vasilenko <com.vasilenko@gmail.com>
2019-02-10 01:42:59 +0300

    Create classes for realization plugin service

---
+++ src/main/java/io/vasilenko/remedy/grunner/service/impl/InlineScriptService.java
@@ -0,0 +1,16 @@
+package io.vasilenko.remedy.grunner.service.impl;
+
+import com.bmc.arsys.api.ARException;
+import com.bmc.arsys.api.Value;
+import io.vasilenko.remedy.grunner.service.PluginService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InlineScriptService implements PluginService {
+
+    @Override
+    public List<Value> run(List<Value> values) throws ARException {
+        return new ArrayList<>();
+    }
+}
