be73ae5140c741aee6c82b7aa1f6b5baa66782d1
Sergey Vasilenko <sergey@vasilenko.io>
2019-03-15 23:36:56 +0300

    Provide remedy and groovy shell

--- src/main/java/io/vasilenko/remedy/grunner/di/InjectorModule.java
+++ src/main/java/io/vasilenko/remedy/grunner/di/InjectorModule.java
@@ -1,8 +1,13 @@
 package io.vasilenko.remedy.grunner.di;
 
+import com.bmc.arsys.api.ARException;
+import com.bmc.arsys.api.ARServerUser;
+import com.bmc.arsys.pluginsvr.ARPluginServerConfiguration;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
@@ -10,6 +15,36 @@ import org.apache.commons.configuration.PropertiesConfiguration;
 
 @Slf4j
 public class InjectorModule extends AbstractModule {
+
+    @Provides
+    @Singleton
+    GroovyShell provideGroovyShell(Binding binding) {
+        GroovyShell groovyShell = new GroovyShell(binding);
+        log.debug("GroovyShell created");
+        return groovyShell;
+    }
+
+    @Provides
+    @Singleton
+    Binding provideBinding(ARServerUser arServerUser) {
+        Binding binding = new Binding();
+        binding.setVariable("ars", arServerUser);
+        log.debug("Binding created");
+        return binding;
+    }
+
+    @Provides
+    @Singleton
+    ARServerUser provideARServerUser(Configuration configuration) {
+        ARServerUser arServerUser = null;
+        try {
+            arServerUser = ARPluginServerConfiguration.getInstance().getARSvrUsr();
+            log.debug("ARServerUser created");
+        } catch (ARException e) {
+            log.error(e.getMessage(), e);
+        }
+        return arServerUser;
+    }
 
     @Provides
     @Singleton
