4174f4e93775a6a0387ddf02c34a72af95323876
Sergey Vasilenko <sergey@vasilenko.io>
2019-03-16 00:10:00 +0300

    Inject groovy shell at service impls

--- src/main/java/io/vasilenko/remedy/grunner/service/impl/FileScriptServiceGrunner.java
+++ src/main/java/io/vasilenko/remedy/grunner/service/impl/FileScriptServiceGrunner.java
@@ -2,12 +2,21 @@ package io.vasilenko.remedy.grunner.service.impl;
 
 import com.bmc.arsys.api.ARException;
 import com.bmc.arsys.api.Value;
+import com.google.inject.Inject;
+import groovy.lang.GroovyShell;
 import io.vasilenko.remedy.grunner.service.GrunnerPluginService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileScriptServiceGrunner implements GrunnerPluginService {
+
+    private final GroovyShell shell;
+
+    @Inject
+    public FileScriptServiceGrunner(GroovyShell shell) {
+        this.shell = shell;
+    }
 
     @Override
     public List<Value> run(List<Value> values) throws ARException {
--- src/main/java/io/vasilenko/remedy/grunner/service/impl/InlineScriptServiceGrunner.java
+++ src/main/java/io/vasilenko/remedy/grunner/service/impl/InlineScriptServiceGrunner.java
@@ -2,12 +2,21 @@ package io.vasilenko.remedy.grunner.service.impl;
 
 import com.bmc.arsys.api.ARException;
 import com.bmc.arsys.api.Value;
+import com.google.inject.Inject;
+import groovy.lang.GroovyShell;
 import io.vasilenko.remedy.grunner.service.GrunnerPluginService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class InlineScriptServiceGrunner implements GrunnerPluginService {
+
+    private final GroovyShell shell;
+
+    @Inject
+    public InlineScriptServiceGrunner(GroovyShell shell) {
+        this.shell = shell;
+    }
 
     @Override
     public List<Value> run(List<Value> values) throws ARException {
